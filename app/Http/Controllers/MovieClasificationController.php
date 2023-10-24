f2953a532a220d5880724f1df54ea758054e10a8
DiegoCampos20151718 <143902081+DiegoCampos20151718@users.noreply.github.com>
2023-10-24 07:27:25 -0600

    fscr de cada tabla

---
+++ app/Http/Controllers/MovieClasificationController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie_Clasification;
+use Illuminate\Http\Request;
+
+class MovieClasificationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Movie_Clasification  $movie_Clasification
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie_Clasification $movie_Clasification)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Movie_Clasification  $movie_Clasification
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movie_Clasification $movie_Clasification)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Movie_Clasification  $movie_Clasification
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie_Clasification $movie_Clasification)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Movie_Clasification  $movie_Clasification
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie_Clasification $movie_Clasification)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/MovieGenderController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie_Gender;
+use Illuminate\Http\Request;
+
+class MovieGenderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Movie_Gender  $movie_Gender
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie_Gender $movie_Gender)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Movie_Gender  $movie_Gender
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movie_Gender $movie_Gender)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Movie_Gender  $movie_Gender
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie_Gender $movie_Gender)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Movie_Gender  $movie_Gender
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie_Gender $movie_Gender)
+    {
+        //
+    }
+}
