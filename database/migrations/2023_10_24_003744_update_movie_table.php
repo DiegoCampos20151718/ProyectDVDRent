1145f59bd314e40fd22bfa16b7e30ca534920093
DiegoCampos20151718 <143902081+DiegoCampos20151718@users.noreply.github.com>
2023-10-23 18:51:09 -0600

    migration update movie y rent_movie

---
+++ database/migrations/2023_10_24_003744_update_movie_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movie', function(Blueprint $table){
+            $table->unsignedBigInteger('gender_id');
+            $table-> foreign('gender_id')
+            -> references('id')-> on('movie_gender')
+            ->onDelete('cascade');
+
+            $table->unsignedBigInteger('clasification_id');
+            $table-> foreign('clasification_id')
+            -> references('id')-> on('movie_clasification')
+            ->onDelete('cascade');
+
+            $table->unsignedBigInteger('category_id');
+            $table-> foreign('category_id')
+            -> references('id')-> on('category')
+            ->onDelete('cascade');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movie', function(Blueprint $table){
+            $table->dropForeign('movie_gender_id_foreign');
+            $table->dropColum('gender_id');
+            $table->dropForeign('movie_clasification_id_foreign');
+            $table->dropColum('clasification_id');
+            $table->dropForeign('movie_category_id_foreign');
+            $table->dropColum('category_id');
+            });
+    }
+};
---
+++ database/migrations/2023_10_24_003809_update_rent_movie_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rent_movie', function(Blueprint $table){
+            $table->unsignedBigInteger('client_id');
+            $table-> foreign('client_id')
+            -> references('id')-> on('client')
+            ->onDelete('cascade');
+
+            $table->unsignedBigInteger('employ_id');
+            $table-> foreign('employ_id')
+            -> references('id')-> on('employ')
+            ->onDelete('cascade');
+
+            $table->unsignedBigInteger('movie_id');
+            $table-> foreign('movie_id')
+            -> references('id')-> on('movie')
+            ->onDelete('cascade');
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rent_movie', function(Blueprint $table){
+            $table->dropForeign('rent_movie_client_id_foreign');
+            $table->dropColum('client_id');
+            $table->dropForeign('rent_movie_employ_id_foreign');
+            $table->dropColum('employ_id');
+            $table->dropForeign('rent_movie_movie_id_foreign');
+            $table->dropColum('movie_id');
+            });
+    }
+};
