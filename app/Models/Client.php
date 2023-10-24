9a1b341484e5435c86c6a28abbf37893250d399e
DiegoCampos20151718 <143902081+DiegoCampos20151718@users.noreply.github.com>
2023-10-24 09:10:59 -0600

    Modelo cliente

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+    protected $fillable =['first_name',
+                    'last_name',
+                    'phone_number',
+                    'address',
+                    'date_of_admission'];
 }
