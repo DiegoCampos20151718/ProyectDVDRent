9f35b221ffd5f0b4ac4b0cababebd5e80cb7d932
DiegoCampos20151718 <143902081+DiegoCampos20151718@users.noreply.github.com>
2023-10-30 10:32:53 -0600

    modificaciones 2

--- resources/js/components/Login.jsx
+++ resources/js/components/Login.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { Button, Form } from 'react-bootstrap'
-import RegisterE from "./RegisterE";
 
 
 function Login() {
