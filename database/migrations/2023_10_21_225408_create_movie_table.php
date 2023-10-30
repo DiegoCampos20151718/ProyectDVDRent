22b020bec0671221cf285d67b414476adf63c2e5
DiegoCampos20151718 <143902081+DiegoCampos20151718@users.noreply.github.com>
2023-10-30 10:40:50 -0600

    modificaciones 3

--- database/migrations/2023_10_21_225408_create_movie_table.php
+++ database/migrations/2023_10_21_225408_create_movie_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->integer('no_copies');
             $table->string('title', 30);
-            $table->image();
             $table->string('actors', 200);
             $table->string('synopsis', 500);
             $table->timestamps();
